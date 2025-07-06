2003415654c1bd59e8971e850c1529c5e0d20368
yasiruLakruwan <yapa1459@gmail.com>
2025-07-06 22:44:52 +0530

    Model training done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,21 +18,68 @@ from xgboost import XGBRegressor
 
 from src.exeption import CustomExeption
 from src.logger import logging
-from src.utils import save_object
+from src.utils import save_object, evaluate_model
 
 @dataclass
 
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join('artifacts','model.pkl')
-    # model saving path and model saving path added here................
-    # move to the model training........................................
+
 
 class ModelTrainer:
     def __init__(self):
         self.modle_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+    def initiate_model_trainer(self,train_array,test_array):
         try:
-            pass
+            logging.info("Spliting training and testing data...!")
+            # train test spliting.......!
+
+            X_train,y_train,X_test,y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Dission Tree": DecisionTreeRegressor(),
+                "Gradiant Boosting":GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "K-Neighbour Classifier":KNeighborsRegressor(),
+                "XGBoost Classifier":XGBRegressor(),
+                "Catboosting Classifier":CatBoostRegressor(),
+                "Adaboost Classifier": AdaBoostRegressor()
+            }
+
+            model_report:dict=evaluate_model(X_train=X_train,X_test=X_test,y_train=y_train,y_test=y_test,
+                                             models= models)
+            
+            # to get the best model score from the dictionary 
+            best_model_score = max(sorted(model_report.values()))
+
+            # to get the best model name from dictionary
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomExeption("No best model found")
+            
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path=self.modle_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test,predicted)
+            
+            return r2_square
+
         except Exception as e:
             raise CustomExeption(e,sys)   
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,12 @@
-import os
-import sys
+# common function add here..................!
 
+import os
+import sys            
 import numpy as np 
 import pandas as pd
 import dill
 import pickle
-#from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score
 #from sklearn.model_selection import GridSearchCV
 
 from src.exeption import CustomExeption
@@ -21,3 +22,24 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomExeption(e, sys)
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            #model = list(models.values())[i]
+
+            model.fit(X_train, y_train) #train the model
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+    except Exception as e:
+        raise CustomExeption(e,sys)
